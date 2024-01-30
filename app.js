2bb8f14847ab9930b9afd26b4a5ead115057dd55
kalkidanteklay <150287564+kalkidanteklay@users.noreply.github.com>
2024-01-30 18:51:19 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const socket = require('socket.io')
+
 const app = express();
-const path = require('path')
-const port = 5000;
-
-//ROUTES
-const indexRouter = require('./routes/index')
-const employerRouter = require('./routes/employers')
-const studentRouter = require('./routes/students')
-const messageRouter = require('./routes/message')
-const profileRouter = require('./routes/profile')
-
-app.use("/", indexRouter);
-app.use("/employers", employerRouter);
-app.use("/students", studentRouter);
-app.use("/message", messageRouter);
-app.use("/profile", profileRouter)
-
-// View engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-
-//START THE SERVER  
-app.listen(port, () => {
-    console.log(`the server is listening at http://localhost: ${port}`)
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.render('index')
 })
+
+
+app.post('/room', (req, res) => {
+    roomname = req.body.roomname;
+    username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//Rooms
+app.get('/room', (req, res)=>{
+    res.render('room')
+})
+
+//Start Server
+const server = app.listen(port, () => {
+    console.log(`Server Running on port ${port}`)
+})
+
+const io = socket(server);
+require('./utils/socket')(io);
---
+++ utils/getUsers.js
@@ -0,0 +1,13 @@
+var users = {}
+
+//Funtion to get users online in a room
+function getUsers(arr){
+    onlineUsers = []
+    if (arr) {
+    arr.forEach((onlineUser) => {
+        onlineUsers.push(Object.values(onlineUser)[0])
+    })
+    return onlineUsers
+}}
+
+module.exports = {getUsers, users};
