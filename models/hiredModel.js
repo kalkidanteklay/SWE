268e1e0f241183d4f4e9732bcd88b270c909a917
abene <abenezertileye2003@gmail.com>
2024-01-31 23:45:03 +0300

    messaging for prototype 2

--- models/hiredModel.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose')
-
-const hiredCandidateSchema = new mongoose.Schema({
-    name: String,
-  });
-  
-const HiredCandidate = mongoose.model('HiredCandidate', hiredCandidateSchema);
-
-module.exports = HiredCandidate
--- models/rejectedModel.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose')
-
-const rejectedCandidateSchema = new mongoose.Schema({
-    name: String,
-  });
-  
-const RejectedCandidate = mongoose.model('RejectedCandidate', rejectedCandidateSchema);
-
-module.exports = RejectedCandidate
